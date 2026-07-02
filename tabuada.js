9f7801460763094248744a6406ce197a187e9be0
Estevao Magliano <ferreiramagliano.info@gmail.com>
2026-07-02 18:32:55 -0300

    correção tabuada

--- tabuada.js
+++ tabuada.js
@@ -1,7 +1,6 @@
-let numero = 1;
+
 for (let i = 1; i <=9; i++){
-    console.log (numero + "x" + i + " = " + (numero *i))
     for (let j = 1; j <= 9; j++){
-        console.log (j + "x" + numero + " = " + (numero *j))
+        console.log (`${i} x ${j} = ${i * j}`);
     }
 }
